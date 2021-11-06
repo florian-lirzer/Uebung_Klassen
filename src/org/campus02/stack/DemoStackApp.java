55caea15088e3b38d9fa7ebb0d4e7ef477742815
florian-lirzer <s50759@edu.campus02.at>
2021-11-06 13:32:31 +0100

    Added DemoStackApp Class and functions

--- src/org/campus02/stack/DemoStackApp.java
+++ src/org/campus02/stack/DemoStackApp.java
@@ -4,6 +4,18 @@ public class DemoStackApp
 {
     public static void main(String[] args)
     {
+        Stack stapel = new Stack(new int [7]);
 
+        stapel.push(7);
+        stapel.push(6);
+        stapel.push(5);
+
+        System.out.println(stapel.pop());
+        System.out.println(stapel.pop());
+
+        stapel.push(3);
+        System.out.println(stapel.pop());
+        System.out.println(stapel.pop());
+        System.out.println(stapel.pop());
     }
 }
